a5eaff8b0d59ab0f284f23d4e2f129b2cffcf130
Minu <122645470+Minusuke@users.noreply.github.com>
2023-09-12 20:25:53 +0000

    primer commit

---
+++ src/js/component/Tablas.jsx
@@ -0,0 +1,25 @@
+// import { faPlay,faForward,faBackward} from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+// const ListaCanciones = ({ canciones }) => {
+//   const baseUrl = "https://assets.breatheco.de/apis/sound/";
+  
+
+//   return (
+//     <>
+//       <div className="titulo">Reproductor De Musica</div>
+//       {canciones.map((cancion) => (
+        
+//         <div className="cancion" key={cancion.id}>
+
+//           <h3>{cancion.name}</h3>
+
+//           <audio src={baseUrl+cancion.url} type="audio/mpeg" />
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export {ListaCanciones};
